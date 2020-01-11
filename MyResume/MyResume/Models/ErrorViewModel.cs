3215b9b9ef2f37cdcb847b9914159ec0cd13b6d2
Richard <rahayn@yahoo.com>
2020-01-11 17:42:02 -0500

    init commit

---
+++ MyResume/MyResume/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyResume.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
